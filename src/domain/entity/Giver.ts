585a1fbdd1088a37951d9546eba62fcd11846815
jeafleohj <jhony.angulo@utec.edu.pe>
2021-04-29 12:17:33 -0500

    Add GIver's constructor

--- src/domain/entity/Giver.ts
+++ src/domain/entity/Giver.ts
@@ -12,7 +12,18 @@ export class Giver {
   event_id: number
   status: string
 
-  constructor({}: Giver) {
-
+  constructor({id, name, lastname, email, document, phone, campaign_id, collection, address, reference, event_id, status}: Giver) {
+    this.id = id
+    this.name = name
+    this.lastname = lastname
+    this.email = email
+    this.document = document
+    this.phone = phone
+    this.campaign_id = campaign_id
+    this.collection = collection
+    this.address = address
+    this.reference = reference
+    this.event_id = event_id
+    this.status = status
   }
 }
