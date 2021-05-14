ecef768bed841ce203f14bfe4c54add04b4ca90a
Valeria <a20151708@pucp.edu.pe>
2021-05-14 00:32:15 -0500

    fix

---
+++ src/application/use_cases/giver/CreateGiver.ts
@@ -0,0 +1,12 @@
+// import { Donation } from 'domain/entity'
+// import { ICampaignRepository } from 'domain/repository'
+
+// const CreateDonation = (data: Donation,
+//   { campaignRepository }: {campaignRepository : ICampaignRepository }): Promise<any> => {
+//   const donation = new Donation(data)
+//   return campaignRepository.addDonation(donation)
+// }
+
+// export {
+//   CreateDonation
+// }
