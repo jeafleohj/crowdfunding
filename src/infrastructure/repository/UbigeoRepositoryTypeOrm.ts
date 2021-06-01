9d57a9487ad4414979cb0e4557d4aeb40d3f6c72
Jhony Angulo <jhony.angulo@utec.edu.pe>
2021-05-31 23:01:46 -0500

    Add province to district

--- src/infrastructure/repository/UbigeoRepositoryTypeOrm.ts
+++ src/infrastructure/repository/UbigeoRepositoryTypeOrm.ts
@@ -6,42 +6,53 @@ import { getRepository, Repository } from 'typeorm'
 import { District, Province } from 'domain/entity/ubigeo'
 
 export class UbigeoRepository implements IUbigeoRepository {
-    private regionRepository: Repository<RegionEntity>
-    private provinceRepository: Repository<ProvinceEntity>
-    private districtRepository: Repository<DistrictEntity>
-    constructor() {
-        this.regionRepository = getRepository(RegionEntity)
-        this.provinceRepository = getRepository(ProvinceEntity)
-        this.districtRepository = getRepository(DistrictEntity)
-    }
+  private regionRepository: Repository<RegionEntity>
+  private provinceRepository: Repository<ProvinceEntity>
+  private districtRepository: Repository<DistrictEntity>
+  constructor() {
+    this.regionRepository = getRepository(RegionEntity)
+    this.provinceRepository = getRepository(ProvinceEntity)
+    this.districtRepository = getRepository(DistrictEntity)
+  }
 
-    getRegions(): Promise<RegionEntity[]> {
-        return this.regionRepository.find()
-    }
+  getRegions(): Promise<RegionEntity[]> {
+    return this.regionRepository.find()
+  }
 
-    getProvinces(id: number): Promise<ProvinceEntity[]> {
-        return this.provinceRepository.find({
-            where: [
-                { region: { id } }
-            ]
-        })
-    }
+  getProvinces(id: number): Promise<ProvinceEntity[]> {
+    return this.provinceRepository.find({
+      where: [
+        { region: { id } }
+      ]
+    })
+  }
 
-    getDistricts(id: number): Promise<DistrictEntity[]> {
-        return this.districtRepository.find({
-            where: [
-                { province: { id } }
-            ]
-        })
-    }
+  getDistricts(id: number): Promise<DistrictEntity[]> {
+    let districts = this.districtRepository.find({
+      where: [
+        { province: { id } }
+      ],
+    })
+    return districts
+  }
 
-    async getDistrict(id: number): Promise<District> {
-        const district = await this.districtRepository.findOne(id)
-        return district as DistrictEntity
-    }
+  async getDistrict(id: number): Promise<District> {
+    const district = await this.districtRepository.findOne({
+      where: [
+        { id }
+      ],
+      join: {
+        alias: 'district',
+        leftJoinAndSelect: {
+          Province: 'district.province',
+        },
+      }
+    })
+    return district as DistrictEntity
+  }
 
-    async getProvince(id: number): Promise<Province> {
-        const province = await this.provinceRepository.findOne(id)
-        return province as ProvinceEntity 
-    }
+  async getProvince(id: number): Promise<Province> {
+    const province = await this.provinceRepository.findOne(id)
+    return province as ProvinceEntity
+  }
 }
