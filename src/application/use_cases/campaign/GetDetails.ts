aa78a554c40760cf64844d6adf62c652260faffa
vale99nadin <valeria123ysg@gmail.com>
2021-06-05 23:53:23 -0500

    get details fixes

--- src/application/use_cases/campaign/GetDetails.ts
+++ src/application/use_cases/campaign/GetDetails.ts
@@ -2,16 +2,17 @@ import { IDonationRepository } from './../../../domain/repository/DonationReposi
 import { ICampaignRepository, ICampaingEventRepository } from 'domain/repository'
 
 const GetDetails = async (id: number,
-  { campaignRepository, donationRepository, campaignEventRepository }: 
+  { campaignRepository, donationRepository, campaignEventRepository }:
   { campaignRepository: ICampaignRepository, donationRepository: IDonationRepository, campaignEventRepository: ICampaingEventRepository })  => {
   //devuelve la campaña con los beneficiarios
-  let campaign = await campaignRepository.listBeneficiaries(id)
+  let campaign = await campaignRepository.getById(id) as any
+  let beneficiaries = await campaignRepository.listBeneficiaries(id)
   let donations = await donationRepository.getByCampaign(id)
   let events = await campaignEventRepository.getByCampaign(id)
 
-  campaign.beneficiaries = campaign.beneficiaries.length
+  campaign.beneficiaries = beneficiaries.length
   campaign.donations = donations
-  campaign.events = events 
+  campaign.events = events
 
   return campaign
 }
