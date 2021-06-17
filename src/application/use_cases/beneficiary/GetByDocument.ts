d54664a399922b8695841d0a161aa9ed24957b5e
Jhony Angulo <jhony.angulo@utec.edu.pe>
2021-06-17 12:16:15 -0500

    Fix types

--- src/application/use_cases/beneficiary/GetByDocument.ts
+++ src/application/use_cases/beneficiary/GetByDocument.ts
@@ -1,7 +1,7 @@
 import { Beneficiary } from 'domain/entity'
 import { IBeneficiaryRepository } from 'domain/repository'
 
-const GetByDocument = (document: number, { beneficiaryRepository }: { beneficiaryRepository: IBeneficiaryRepository }): Promise<Beneficiary> => {
+const GetByDocument = (document: string, { beneficiaryRepository }: { beneficiaryRepository: IBeneficiaryRepository }): Promise<Beneficiary> => {
   return beneficiaryRepository.getByDocument(document)
 }
 
