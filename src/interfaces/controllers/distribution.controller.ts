0dbbe7707b700cdbd17439ef0d0ee75c1ed1aebe
Jhony Angulo <jhony.angulo@utec.edu.pe>
2021-07-18 20:04:21 -0500

    Fix maxlimit on distribution

--- src/interfaces/controllers/distribution.controller.ts
+++ src/interfaces/controllers/distribution.controller.ts
@@ -142,6 +142,16 @@ const updateDistributionByBeneficiary = async (ctx: Context) => {
     amount: number,
   }>
 
+  let notValids = amounts.map( el => el.amount == 0 ? true : false )
+  let valid = notValids.reduce((prev, el) => prev && el, true)
+
+  if( valid ) {
+    throw new ErrorHandler({
+      status: 400,
+      message: 'Todos los campos son ceros',
+    })
+  }
+
   let distribution = amounts.map(amount => {
     return {
       amount: amount.amount,
@@ -165,7 +175,7 @@ const updateDistributionByBeneficiary = async (ctx: Context) => {
   let rest = currentDistribution.map((el, index) => {
     let newAmount = el.total - beneficiaryDistribution[index].amount + amounts[index].amount
     let collected = listDonations[index].collected
-    if ( newAmount >= collected )  {
+    if ( newAmount > collected )  {
       throw new ErrorHandler({
         status: 400,
         message: 'Valores incorrectos',
